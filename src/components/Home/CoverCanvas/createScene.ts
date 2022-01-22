b0f576a585ecde3077bda2cc63980d46c16be8f2
Xiaoxi <yxx0827@gmail.com>
2022-01-22 00:42:29 -0500

    update

--- src/components/Home/CoverCanvas/createScene.ts
+++ src/components/Home/CoverCanvas/createScene.ts
@@ -30,8 +30,8 @@ const createScene = async function (engine: BABYLON.Engine, canvas: HTMLCanvasEl
   const importedCamera = scene.getCameraByName("Camera");
   const importedCameraTarget = scene.getMeshByName('cameraTarget');
   //@ts-ignore
-  // importedCamera.lockedTarget = importedCameraTarget;
-  importedCamera?.attachControl()
+  importedCamera.lockedTarget = importedCameraTarget;
+  // importedCamera?.attachControl()
 
   // lights
   const light = new BABYLON.DirectionalLight('Sun', new BABYLON.Vector3(-2, -1, 2.5), scene);
