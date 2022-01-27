8dd29a7c3a16f3f59555bf16c8dbc2a649f7b22e
Xiaoxi <yxx0827@gmail.com>
2022-01-27 00:44:55 -0500

    Revert "try remove unneccessary camera from pipeline"
    This reverts commit 6248f16ffca404e64f0a3d288f07432c996f5727.

--- src/components/Home/CoverCanvas/cameras.ts
+++ src/components/Home/CoverCanvas/cameras.ts
@@ -52,19 +52,13 @@ const initCameras = (scene: BABYLON.Scene) => {
   container.addControl(button);
   advancedTexture.addControl(container);
 
-  // postEffects
-  const pipeline = initPostEffects(scene);
-  pipeline.addCamera(allCameras[activeCameraIndex]);
-
   // add listener to button
   button.onPointerClickObservable.add(() => {
-    pipeline.removeCamera(allCameras[activeCameraIndex]);
     activeCameraIndex++;
     if (activeCameraIndex === allCameras.length) {
       activeCameraIndex = 0;
     }
     switchCamera(activeCameraIndex);
-    pipeline.addCamera(allCameras[activeCameraIndex]);
   })
 
   return allCameras
@@ -127,45 +121,6 @@ const getAirShipCamera = (scene: BABYLON.Scene) => {
   camera.fov = 1.5;
 
   return camera
-}
-
-const initPostEffects = (scene: BABYLON.Scene) => {
-  // post fx
-  // volumetric light
-  const sunMesh = BABYLON.Mesh.CreatePlane("sunMesh", 3, scene);
-  const sunMeshMaterial = new BABYLON.StandardMaterial('sunSourceMaterial', scene);
-  sunMeshMaterial.emissiveColor = BABYLON.Color3.FromHexString('#fbe9e7');
-  //@ts-ignore
-  sunMeshMaterial.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
-  sunMeshMaterial.diffuseTexture = new BABYLON.Texture("/assets/sun.png", scene);
-  sunMeshMaterial.diffuseTexture.hasAlpha = true;
-  sunMesh.material = sunMeshMaterial;
-  sunMesh.position = new BABYLON.Vector3(-10, 0.5, 100);
-  // const vls = new BABYLON.VolumetricLightScatteringPostProcess('godrays', 1, allCameras[0], sunMesh, 100, BABYLON.Texture.BILINEAR_SAMPLINGMODE, engine, false);
-  // vls.useDiffuseColor = true;
-  // vls.exposure = 0.8;
-  // vls.samples = 4;
-
-  //render pipeline
-  const pipeline = new BABYLON.DefaultRenderingPipeline(
-    "defaultPipeline", // The name of the pipeline
-    true, // Do you want the pipeline to use HDR texture?
-    scene, // The scene instance
-    [] // The list of cameras to be attached to
-  );
-  pipeline.imageProcessing.vignetteEnabled = true;
-  pipeline.imageProcessing.vignetteColor = new BABYLON.Color4(0,0,0, 1);
-  pipeline.imageProcessing.vignetteWeight = 10;
-  pipeline.samples = 4;
-  // pipeline.fxaaEnabled = true;
-  pipeline.bloomEnabled = true;
-  pipeline.bloomWeight = 0.1;
-  pipeline.sharpenEnabled = true;
-  pipeline.sharpen.edgeAmount = 0.2; // default 0.3
-  pipeline.imageProcessing.contrast = 1; // default 1
-  pipeline.imageProcessing.exposure = 1; // default 1
-
-  return pipeline
 }
 
 export default initCameras
