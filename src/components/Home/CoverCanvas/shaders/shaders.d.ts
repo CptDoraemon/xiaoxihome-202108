c30b1c057e6394ea2e8a1b7230fc9d52bf5b2b02
Jake Yu <yxx0827@gmail.com>
2025-11-27 23:02:27 -0500

    fix type

---
+++ src/components/Home/CoverCanvas/shaders/shaders.d.ts
@@ -0,0 +1,14 @@
+declare module '*.glsl' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.vs' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.fs' {
+  const content: string;
+  export default content;
+}
