a1199eb1ecdb5d6001fd12d842ff30f461907010
Jake Yu <yxx0827@gmail.com>
2025-12-26 23:16:26 -0500

    bump nextjs version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    rules: {
+      '*.glsl': { loaders: ['raw-loader'], as: '*.js' },
+      '*.vs': { loaders: ['raw-loader'], as: '*.js' },
+      '*.fs': { loaders: ['raw-loader'], as: '*.js' },
+      '*.vert': { loaders: ['raw-loader'], as: '*.js' },
+      '*.frag': { loaders: ['raw-loader'], as: '*.js' },
+    },
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
